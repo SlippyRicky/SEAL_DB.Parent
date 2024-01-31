200f853f41a1fa18a8276d5e4b9d2846bd6c0ec5
SlippyRicky <122894088+SlippyRicky@users.noreply.github.com>
2024-01-31 20:40:58 +0100

    deleted sneaky untitled.R

--- SEAL_DB.Rapp/UntitledR.R
+++
@@ -1,6 +0,0 @@
-output$selectedImage <- renderImage({
-   selectedImage <- paste0(input$image, ".jpg")
-   imagePath <- file.path("/www", selectedImage)
-   print(imagePath)  # Add this line for debugging
-   return(list(src = imagePath, contentType = "image/jpg"))
-}, deleteFile = FALSE)
